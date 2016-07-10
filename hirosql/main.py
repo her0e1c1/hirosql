e7f8d5194312151c1b7b5d62994a2a0bf9a7a213
her0e1c1 <her0e1c1@gmail.com>
2016-07-10 07:22:35 +0000

    add update statement

--- hirosql/main.py
+++ hirosql/main.py
@@ -25,10 +25,18 @@ def build_delete(table_name):
     return "delete from {table_name} {where};".format(**locals())
 
 
+def build_update(table_name):
+    where = build_where()
+    set_ = " set "
+    if request.forms:
+        set_ += ",".join([u" `%s`='%s' " % (k, v) for k, v in request.forms.allitems() if v != ''])
+    return "update {table_name} {set_} {where};".format(**locals())
+
+
 def build_where():
     where = ""
     if request.params:
-        where = "where " + " or ".join("`%s`='%s'" % (k, v) for k, v in request.params.allitems())
+        where = "where " + " or ".join("`%s`='%s'" % (k, v) for k, v in request.query.allitems())
     return where
 
 
@@ -38,7 +46,7 @@ TEMPLATE_TABLE = u"""
 var f=$("form[method=get]");
 $("input").on("click", function(evt){ var i=$(this); var input=$("<input>").val(i.val()).attr("name", i.attr("name")); f.append(input)});
 })</script>
-<a href="/">/</a><br/>
+<a href="/">top</a><br/>
 <a href="/{{ database_name }}">{{ database_name }}</a><br/>
 <a href="{{ request.path }}">{{ request.path }}</a><br/>
 {{ query }}</br>
@@ -46,9 +54,23 @@ $("input").on("click", function(evt){ var i=$(this); var input=$("<input>").val(
 <form method="post" action="{{ request.path + '/delete?' + request.query_string }}"
       onsubmit="return confirm('Do you really want to delete this ?');">
 <input type="submit" value="delete"></form>
+
 <form method="get" action="{{ request.path }}"><input type="submit" value="filter"></form>
 <table>
-<tr>{% for k in keys %}<td>{{ k }}</td>{% endfor %}</tr>
+
+<tr>{% for k in keys %}<td>{{ k }}</td>{% endfor %}<td>action</td></tr>
+
+<tr>
+<form id="update" method="post" action="{{ url_update }}"
+      onsubmit="return confirm('Do you really want to delete this ?');">
+    {% for k in keys %}
+      <td><input name='{{ k }}' value=''></td>
+    {% endfor %}
+    <td><input type="submit" value="update"></td>
+</form>
+<td onclick="$.post('{{ url_update_query }}', $('form#update').serializeArray()).done(function(data){ alert(data); })" >confirm</td>
+</tr>
+
 {% for row in rows %}
 <tr>
   {% for r in row %}
@@ -85,6 +107,18 @@ def database(database_name):
     """).render(rows=rows, database_name=database_name)
 
 
+@post("/<database_name>/<table_name>/update")
+def update_rows(database_name, table_name):
+    if not all([v == '' for  k, v in request.forms.allitems()]):
+        get_conn(database_name).execute(build_update(table_name))
+    redirect("/%s/%s" % (database_name, table_name))
+
+
+@post("/<database_name>/<table_name>/update/query")
+def update_rows_query(database_name, table_name):
+    return build_update(table_name)
+
+
 @post("/<database_name>/<table_name>/delete")
 def delete_rows(database_name, table_name):
     if request.params:
@@ -98,8 +132,12 @@ def table(database_name, table_name):
     e = get_conn(database_name).execute(query)
     keys = e.keys()
     rows = e.fetchall()
+    url_update = request.path + '/update?' + request.query_string
+    url_update_query = request.path + '/update/query?' + request.query_string
     return jinja2.Template(TEMPLATE_TABLE).render(
         database_name=database_name,
+        url_update=url_update,
+        url_update_query=url_update_query,
         rows=rows,
         keys=keys,
         query=query,
